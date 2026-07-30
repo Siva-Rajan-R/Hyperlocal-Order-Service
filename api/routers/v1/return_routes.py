0f37c9883865c64e884af8daefb41ea81854e9ba
Siva-Rajan-R <siva967763@gmail.com>
2026-07-30 22:19:27 +0530

    implemented the purchase return

--- api/routers/v1/return_routes.py
+++ api/routers/v1/return_routes.py
@@ -14,6 +14,13 @@ from core.utils.user_info import get_current_user_id
 PG_SESSION = Annotated[AsyncSession, Depends(get_pg_async_session)]
 SHOP_ID = "37d5519b-51a1-5854-982b-4d6524171017" # Consistent with existing implementation
 
+@router.post('/{shop_id}')
+async def create_return_by_shop_id(shop_id: str, data: CreateReturnSchema, session: PG_SESSION, user_id: Optional[str] = Depends(get_current_user_id)):
+    data.shop_id = shop_id
+    return await HandleReturnRequest(session=session, shop_id=shop_id, cur_user_id=user_id or "").create(data=data)
+
 @router.post('')
 async def create_return(data: CreateReturnSchema, session: PG_SESSION, user_id: Optional[str] = Depends(get_current_user_id)):
-    return await HandleReturnRequest(session=session, shop_id=SHOP_ID, cur_user_id=user_id or "").create(data=data)
+    shop_id = data.shop_id or SHOP_ID
+    data.shop_id = shop_id
+    return await HandleReturnRequest(session=session, shop_id=shop_id, cur_user_id=user_id or "").create(data=data)
